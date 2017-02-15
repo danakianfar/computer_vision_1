27aadf675a9e52e2a6e587cd570356800370050c
Jose Gallego <jgalle29@gmail.com>
2017-02-16 00:26:27 +0100

    Corrected visualization of each individual channel

--- lab1/colorspace/ConvertColorSpace.m
+++ lab1/colorspace/ConvertColorSpace.m
@@ -19,14 +19,18 @@ input_image = im2double(input_image);
 
 if strcmp(colorspace, 'opponent')
     new_image = rgb2opponent(input_image); % fill in this function
+    labels = {'O1', 'O2', 'O3'};
 elseif strcmp(colorspace, 'rgb')  
     new_image = rgb2normedrgb(input_image); % fill in this function
+    labels = {'Normalized R', 'Normalized G', 'Normalized B'};
 elseif strcmp(colorspace, 'hsv')   
     % use the built-in function
     new_image = hsv2rgb(input_image);
+    labels = {'H', 'S', 'V'};
 elseif strcmp(colorspace, 'ycbcr')
     % use the built-in function
     new_image = rgb2ycbcr(input_image);
+    labels = {'Y', 'Cb', 'Cr'};
 elseif strcmp(colorspace, 'gray')
     [out_light, out_avg, out_lum, out_matlab] = rgb2grays(input_image); % fill in this function
     new_image = {out_light, out_avg, out_lum, out_matlab};
@@ -41,7 +45,7 @@ if strcmp(colorspace, 'gray')
     visualize(out_light, out_avg, out_lum, out_matlab, ...
         {'lightness', 'average', 'luminosity', 'matlab'});
 else
-    visualize(new_image, {colorspace});
+    visualize(new_image(:,:,1), new_image(:,:,2), new_image(:,:,3), labels);
 end
 
 end
--- lab1/colorspace/visualize.m
+++ lab1/colorspace/visualize.m
@@ -2,14 +2,16 @@ function visualize(varargin)
 % Displays a set of images. The format should be 
 % visualize(im_1, ..., im_n, {'label_1', ..., 'label_n'})
 
-if nargin == 2
+if nargin <= 3
     row_num = 1;
+    col_num = nargin-1;
 else
     row_num = 2;
+    col_num = ceil((nargin-1)/2);
 end
 
 for i = 1:nargin-1
-    subplot(row_num, ceil((nargin-1)/2),i);
+    subplot(row_num, col_num, i);
     imshow(varargin{i});
     title(varargin{end}(i))
 end
