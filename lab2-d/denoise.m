0343bb79717dfb0581693d2a9eec7b0bb5654e04
Dana Kianfar <dana.kianfar@gmail.com>
2017-02-21 16:07:53 +0100

    lab 2

---
+++ lab2-d/denoise.m
@@ -0,0 +1,23 @@
+function imout = denoise (image , kernel_type , kernel_size, boundary_padding)
+    img = im2double(image);
+    
+    pad = floor(kernel_size/2);
+    img = padarray(img, [pad pad] , boundary_padding, 'both');
+    
+    imout = zeros(size(img));
+    
+    if strcmp(kernel_type,'median')
+        f = @(x) median(x(:));
+    elseif strcmp(kernel_type,'box') 
+        f = @(x) ones(size(x(:))) / numel(x(:));
+    end
+    
+    for i=1:size(image,3)
+        x = squeeze(img(:,:,i));
+        %imout(:,:,i) = colfilt(x , [kernel_size kernel_size], 'sliding', f);
+        imout(:,:,i) = nlfilter(x , [kernel_size kernel_size], f);
+    end
+    
+    % remove padding
+    imout = imout(pad+1:end-pad,pad+1:end-pad,:);
+end
