0d255aae5cbd0e0c177ce19cb479b2545c841d49
Dana Kianfar <dana.kianfar@gmail.com>
2017-03-02 09:05:14 +0100

    lab3 pt1

--- lab3/code/harris.m
+++ lab3/code/harris.m
@@ -1,8 +1,8 @@
-function [ H, r, c, Ix, Iy] = harris(I, K, R, N)
+function [ H, r, c, Ix, Iy] = harris(I, K, sigma, R, N)
     
     % Creates a gaussian filter G and its x and y derivatives
     G = fspecial('gaussian',[K K]);
-    [Gx,Gy] = gradient(G);
+    [Gx,Gy] = gradient(G, sigma);
 
     % Calculates the derivatives of the smoothed image
     Ix = imfilter(I, Gx, 'replicate', 'same',  'conv');
@@ -16,28 +16,12 @@ function [ H, r, c, Ix, Iy] = harris(I, K, R, N)
     % Calculates 'cornerness'
     H = (A .* C) - (B .^ 2) - 0.04 * (A + C) .^ 2;
     
-    % Apply threshold and non maximal supression
-    H = nonmaxsupp(H, N, R);
+    % Threshold values
+    H = H .* double(H > R); 
     
-    %H = H .* (H>R);
+    % Apply non maximal supression to non-overlapping neighborhoods
+    H = nonmaxsupp(H, N);
     
     [c, r] = find(H);
     
 end
-
-function [maxima] = nonmaxsupp(X, N, R)
-
-    h = ones(N);
-    % Center of filter is 0 as we are replacing that value
-    h(ceil((N*N)/2)) = 0; 
-
-    bin_max = ordfilt2(X, (N ^ 2) - 1, h);
-    maxima = (X > bin_max) & (X > R);
-
-    % Corners are unreliable as they are padded
-    maxima(1:N,1:N) = 0;
-    maxima(end - N:end, 1:N) = 0;
-    maxima(1:N,end - N:end) = 0;
-    maxima(end - N:end,end - N:end) = 0;
-
-end 
--- lab3/code/main.m
+++ lab3/code/main.m
@@ -1,5 +1,5 @@
 %% Computer Vision 1 
-% Lab 2: Filters
+% Lab 3: Edge Detection and Motion Tracking
 % Authors: Dana Kianfar - Jose Gallego
 
 %% Harris Corner Detector
@@ -8,11 +8,17 @@ I = imread('../person_toy/00000001.jpg');
 % Convert RGB data to instensity values
 I = rgb2gray(im2double(I));
 
-[ H, r, c, Ix, Iy] = harris(I, 3, 0.2, 0.00003, 5);
+sigma = 1; % Gaussian kernel sigma
+K = 5; % Gaussian kernel width
 
-imshow(I);
-hold on
-plot(r,c,'r*');
+% Non-Maximal Supression and Threshold
+N = 1; % neighborhood window size (converted to 2N+1)
+threshold = 5e-6; % threshold for minimum R value
+
+[ H, r, c, Ix, Iy] = harris(I, K, sigma, threshold, N);
+
+figure, subplot(2,2,1:2), imshowpair(I, I, 'montage'); hold on; plot(r,c,'r*'); hold off; title(compose('Corners (left) and Original (right), n=%d, t=%.6f, s=%d, k=%d', 2*N+1, threshold, sigma, K), 'FontSize', 15);
+subplot(2,2,3:4), imshowpair(Ix,Iy, 'montage' ); title('Gradient-x (left) and Gradient-y (right)', 'FontSize', 15);
 
 %%
 
