53b204cf42d33791c251dca3056e29ab4b124e5c
Dana Kianfar <dana.kianfar@gmail.com>
2017-03-05 16:55:10 +0100

    wut wut

--- lab2/code/myHistMatching.m
+++ lab2/code/myHistMatching.m
@@ -11,28 +11,6 @@ function imOut = myHistMatching(input, reference)
 end
 
 function out = hist_matching(im1, im2)
-M = zeros(256,1,'uint8'); % Mapping for each bin in CDF. No need for converting to double here
-
-% Get histograms
-hist1 = imhist(im1);
-hist2 = imhist(im2);
-
-% Compute CDFs
-cdf1 = cumsum(hist1) / numel(im1); 
-cdf2 = cumsum(hist2) / numel(im2);
-
-% Compute mapping for each bin
-for i = 1 : 256
-    [~,ind] = min(abs(cdf1(i) - cdf2)); % find the bin with the smallest intensity difference
-    M(i) = ind-1; % decrement range [1,256] to match uint8 [0,255]
-end
-
-%// Now apply the mapping to get first image to make
-%// the image look like the distribution of the second image
-out = M(double(im1)+1); % increment to get recover RGB trips
-end
-
-function out = hist_matching2(im1, im2)
     % Compute CDF of both images
     cdf1 = cumsum(imhist(im1)) / numel(im1); 
     cdf2 = cumsum(imhist(im2)) / numel(im2);
