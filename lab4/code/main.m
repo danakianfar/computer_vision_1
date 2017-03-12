b6f00e186abc2e9c8aa2caab255255373a247b57
Dana Kianfar <dana.kianfar@gmail.com>
2017-03-12 14:28:40 +0100

    rgb2grayt correction

--- lab4/code/main.m
+++ lab4/code/main.m
@@ -7,28 +7,36 @@
 
 clear, clc, close all
 
-left = im2single(imread('./images/left.jpg'));
-right = im2single(imread('./images/right.jpg'));
+% im1= im2single(imread('./images/left.jpg'));
+% im2 = im2single(imread('./images/right.jpg'));
 
-% left = im2single(imread('./images/boat1.pgm'));
-% right = im2single(imread('./images/boat2.pgm'));
+im1 = im2single(imread('./images/boat1.pgm'));
+im2 = im2single(imread('./images/boat2.pgm'));
+
+if size(im1,3) > 1
+   left = rgb2gray(im1);
+   right = rgb2gray(im2);
+else
+   left = im1;
+   right = im2;
+end
 
 % Detect interest points in each image and their descriptors
-[F1, D1] = vl_sift(rgb2gray(left));
-[F2, D2] = vl_sift(rgb2gray(right));
+[F1, D1] = vl_sift(left);
+[F2, D2] = vl_sift(right);
 
 % Get the set of supposed matches between region descriptors in each image
 M = vl_ubcmatch(D1, D2); 
 
 % Randomly plot 50 matches
-%[f1_selection, f2_selection] = plot_sample_matches( left, right, M, F1, F2, 50);
+% [f1_selection, f2_selection] = plot_sample_matches(left, right, M, F1, F2, 50);
  
 % Execute RANSAC to get best match
 p = 0.95; % confidence
 [W, T]= ransac(F1, F2, M, p);
 
 % Plot results
-%plot_ransac_results( left, right, f1_selection, f2_selection, W, T )
+% plot_ransac_results( left, right, f1_selection, f2_selection, W, T )
 
 %%
 neighbors = 4;
