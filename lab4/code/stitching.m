8dd274f8d475814309afefb15d88c08a31ec070b
Jose Gallego <jgalle29@gmail.com>
2017-03-12 18:01:12 +0100

    Fix stitching alignment

--- lab4/code/stitching.m
+++ lab4/code/stitching.m
@@ -23,12 +23,10 @@ neighbors = 1;
 nn_filt = @(x) mean(x);
 [t_image, z_bounds] = transform_image(im2, W, neighbors, false, nn_filt);
 
-%imshow(t_image);
-
 [h,w,n_channels] = size(im1);
 
 x_bounds = [ 1 1 h h; 1 w 1 w];
-w_bounds = [x_bounds, z_bounds - round([T(2) - min(z_bounds(1,:)); T(1) - min(z_bounds(2,:))])];
+w_bounds = [x_bounds, z_bounds  - round([T(2) - min(z_bounds(1,:)); T(1) - min(z_bounds(2,:))])];
 
 [min_vals, ~ ] = min(w_bounds');
 min_r = min_vals(1);
@@ -41,7 +39,7 @@ max_c = max_vals(2);
 q_bounds = w_bounds - [min_r; min_c] + 1;
 
 Z = zeros([min(q_bounds(1,:)), min(q_bounds(2,:)), n_channels]);
-Z(min(q_bounds(1,5:8)) : max(q_bounds(1,5:8)), min(q_bounds(2,5:8)) : max(q_bounds(2,5:8)), :) = t_image;
+Z(min(q_bounds(1,5:8)): max(q_bounds(1,5:8)), min(q_bounds(2,5:8)) : max(q_bounds(2,5:8)), :) = t_image;
 Z(min(q_bounds(1,1:4)) : max(q_bounds(1,1:4)), min(q_bounds(2,1:4)) : max(q_bounds(2,1:4)), :) = im1;
 
 end
